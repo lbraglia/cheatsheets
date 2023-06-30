b264772a3c336d8898486597538285ddca36a17c
Luca Braglia <lbraglia@gmail.com>
2023-06-30 11:56:11 +0200

    update

---
+++ python_r.md
@@ -0,0 +1,15 @@
+# R - Python
+
+
+## Data Misc
+
+| R                 | Python                             |
+|-------------------|------------------------------------|
+| `source('foo.R')` | `execfile('foo.py') or run foo.py` |
+
+## Data Second
+
+| R     | Python   |
+|-------|----------|
+| `asd` | `foo`    |
+
---
+++ src/python_r.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from pathlib import Path
+import pylbmisc as lb
+
+infile = Path("python_r_data.xlsx")
+dfs = lb.io.data_import(infile)
+
+def add_code_markup(x, lang = ''):
+    return "`" + x.astype(str) + "`"
+
+md = ["# R - Python", "\n\n\n"]
+for nm, df in dfs.items():
+    title = nm.replace("python_r_", "").replace("_", " ").title()
+    md.append("## {}\n\n".format(title))
+    # add code for content to R and Python
+    df["R"] = add_code_markup(df["R"])
+    df["Python"] = add_code_markup(df["Python"])
+    md.append(df.to_markdown(index=False, tablefmt="github"))
+    md.append("\n\n")
+
+    
+outfile = Path("../python_r.md")
+with outfile.resolve().open(mode="w") as f:
+    f.writelines(md)
